8b52c6adc65401475920d3b9c8deba90397f2d21
kotathefriends <5070kouta@gmail.com>
2023-12-02 20:34:07 +0900

    配列からランダムな要素を選択

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var names: [String] = ["Elisha", "Andre", "Jasmine", "Po-Chun"]
+    @State private var nameToAdd = ""
+    @State private var pickedName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(pickedName.isEmpty ? " " : pickedName)
+            
+            List {
+                ForEach(names, id: \.self) { name in
+                    Text(name)
+                }
+            }
+            TextField("Add Name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Button("Pick Random Name") {
+                if names.count > 0 {
+                    pickedName = names.randomElement()!
+                } else {
+                    pickedName = ""
+                }
+            }
         }
         .padding()
     }

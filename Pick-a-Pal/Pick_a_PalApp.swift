dfcbc8584e8ecc5324bffead4d4043914c41624a
kotathefriends <5070kouta@gmail.com>
2023-12-02 18:01:50 +0900

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by KOTA TAKAHASHI on 2023/12/02.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
